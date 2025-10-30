fe1ac4383ab329fe66805958b845b744aa3dd361
ChrisVeigas <christineveigas033@gmail.com>
2025-10-30 15:09:20 +0530

    Updated Zod features

--- src/hooks/useRegisterForm.js
+++ src/hooks/useRegisterForm.js
@@ -4,11 +4,11 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
 import { logger } from "../utils/logger";
-import { ZodArray } from "zod/v3";
 
 const registerSchema = z.object({
   username: z.string().min(1, "Username is mandatory"),
-  email: ZodArray
+  email: z
+    .string()
     .min(1, "Email is required")
     .email("Must be a valid email with @"),
   password: z
